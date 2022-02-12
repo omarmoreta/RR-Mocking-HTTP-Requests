48a4950fd92f9d29d1be15616163dbeae842c623
Omar <Omar>
2022-02-12 02:07:55 -0500

    added tests for github link and image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react'
-import './App.css'
-
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [gitHubName, setGitHubName] = useState("");
+  const [gitHubURL, setGitHubURL] = useState("");
+  const [gitHubImageURL, setGitHubImageURL] = useState("");
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/omarmoreta")
+      .then((res) => res.json())
+      .then((data) => {
+        setGitHubName(data.name);
+        setGitHubURL(data.html_url);
+        setGitHubImageURL(data.avatar_url);
+      });
+  }, []);
+
   return (
     <div className="App">
       <h1>Github Profile Info:</h1>
+      <h2>{gitHubName}</h2>
+      <a href={gitHubURL}>
+        <button>GitHub Page</button>
+      </a>
+      <img src={gitHubImageURL} alt="Github profile image" />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,40 @@
+//Write a test that mock fetches data from the GitHub API
+import App from "./App";
+import { render, screen, waitFor } from "@testing-library/react";
 
+//Resets everything to initial state before running test
+beforeEach(() => {
+  fetch.resetMocks();
+});
 
+test("receives GitHub name from GitHub REST API using jest fetch mock", async () => {
+  fetch.mockResponseOnce(JSON.stringify({ name: "Omar Moreta" }));
+  render(<App />);
+  const gitHubName = await waitFor(() =>
+    screen.getByRole("heading", { level: 2 })
+  );
+  expect(gitHubName).toHaveTextContent("Omar Moreta");
+});
 
+test("receives GitHub profile URL from GH API", async () => {
+  fetch.mockResponseOnce(
+    JSON.stringify({ html_url: "https://github.com/omarmoreta" })
+  );
+  render(<App />);
+  const gitHubLink = await waitFor(() => screen.getByRole("link"));
+  expect(gitHubLink).toHaveAttribute(
+    "href",
+    expect.stringContaining("https://github.com/omarmoreta")
+  );
+});
+
+test("receives GitHub profile Image URL from GH API", async () => {
+  fetch.mockResponseOnce(
+    JSON.stringify({
+      avatar_url: "https://avatars.githubusercontent.com/u/88066217?v=4",
+    })
+  );
+  render(<App />);
+  const gitHubURL = await waitFor(() => screen.getByRole("img"));
+  expect(gitHubURL).toHaveAttribute("src", expect.stringContaining("avatars"));
+});
